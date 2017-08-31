18cf802020f25a49c77733b90c7f42e4d9b8a6fe
Kevin Suwala <kevin@suwala.me>
2017-08-30 22:48:42 -0700

    Fix issue with how dates are formated and parsed

--- server/data.js
+++ server/data.js
@@ -30,22 +30,22 @@ const data = {
     {
       categories: ["China120R", "CS350"],
       description: "fun",
-      startTime: 1492834595,
-      endTime: 1492902540,
+      startTime: 1492834595000,
+      endTime: 1492902540000,
       UserId: 1
     },
     {
       categories: ["CS341"],
       description: "hard",
-      startTime: 1492845395,
-      endTime: 1492852595,
+      startTime: 1492845395000,
+      endTime: 1492852595000,
       UserId: 1
     },
     {
       categories: ["MSCI311", "Stat231"],
       description: "boring",
-      startTime: 1492874195,
-      endTime: 1492876535,
+      startTime: 1492874195000,
+      endTime: 1492876535000,
       UserId: 1
     }
   ]
--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,20 @@
 var moment = require('moment');
 
 export function formattedTimeDifference(start, end) {
-  var startTime = moment.unix(start);
-  var endTime = moment.unix(end);
+  var startTime = moment(start);
+  var endTime = moment(end);
 
   var ms = endTime.diff(startTime);
   var d = moment.duration(ms);
   var s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
-
+  
   return s;
 }
 
 export function formattedTimeRange(start, end) {
-  var startTime = moment.unix(start).format("HH:mm:ss");
-  var endTime = moment.unix(end).format("HH:mm:ss");
-
+  var startTime = moment(start).format("HH:mm:ss");
+  var endTime = moment(end).format("HH:mm:ss");
+  var temp = startTime;
+  
   return startTime + "-" + endTime;
 }
